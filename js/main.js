967a43433fb3e75fc224526ff5afd93a370cb8d6
Alexis Vilaça <cpm.alexisvilaca@gmail.com>
2022-08-04 18:43:05 +0100

    Mouse hover trail effect

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
     mainContainer.appendChild(squareDiv);
+    squareDiv.addEventListener("mouseover", () => {
+        squareDiv.style.backgroundColor = "#4b6ece";
+    })
 }
+
